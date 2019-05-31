72c31002f79aa2552c1c8441d95dc5598557f49e
Timothyholmes11 <tpholmes7@gmail.com>
2019-05-31 12:32:10 -0500

    Update

--- heat_map.py
+++ heat_map.py
@@ -1,14 +1,15 @@
+#! /usr/bin/env python3
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
-DAYS = ['Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thurs.', 'Fri.', 'Sat.']
-MONTHS = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.']
+days = ['Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thurs.', 'Fri.', 'Sat.']
+months = ['J.', 'F.', 'M.', 'A.', 'M.', 'J.', 'J.', 'A.', 'S.', 'O.', 'N.', 'D.']
 
 
-def date_heatmap(series, start=None, end=None, mean=False, ax=None, **kwargs):
+def calendar_heatmap(series, start=None, end=None, mean=False, ax=None, **kwargs):
     '''Plot a calendar heatmap given a datetime series.
 
     Arguments:
@@ -41,142 +42,67 @@ def date_heatmap(series, start=None, end=None, mean=False, ax=None, **kwargs):
     group = series.groupby(dates)
     series = group.mean() if mean else group.sum()
 
-    # Parse start/end, defaulting to the min/max of the index.
     start = pd.to_datetime(start or series.index.min())
     end = pd.to_datetime(end or series.index.max())
 
-    # We use [start, end) as a half-open interval below.
     end += np.timedelta64(1, 'D')
 
-    # Get the previous/following Sunday to start/end.
-    # Pandas and numpy day-of-week conventions are Monday=0 and Sunday=6.
     start_sun = start - np.timedelta64((start.dayofweek + 1) % 7, 'D')
     end_sun = end + np.timedelta64(7 - end.dayofweek - 1, 'D')
-    start_year = start - np.timedelta64((start.dayofyear))
-    end_year = end + np.timedelta64(end.dayofyear)
 
-    # Create the heatmap and track ticks.
     num_weeks = (end_sun - start_sun).days // 7
     heatmap = np.zeros((7, num_weeks))
-    ticks = {}  # week number -> month name
+    ticks = {} 
     for week in range(num_weeks):
         for day in range(7):
             date = start_sun + np.timedelta64(7 * week + day, 'D')
             if date.day == 1:
-                ticks[week] = MONTHS[date.month - 1]
+                ticks[week] = months[date.month - 1]
             if date.dayofyear == 1:
                 ticks[week] += f'\n{date.year}'
             if start <= date < end:
                 heatmap[day, week] = series.get(date, 0)
 
-    # Get the coordinates, offset by 0.5 to align the ticks.
     y = np.arange(8) - 0.5
     x = np.arange(num_weeks + 1) - 0.5
 
-    # Plot the heatmap. Prefer pcolormesh over imshow so that the figure can be
-    # vectorized when saved to a compatible format. We must invert the axis for
-    # pcolormesh, but not for imshow, so that it reads top-bottom, left-right.
     ax = ax or plt.gca()
     mesh = ax.pcolormesh(x, y, heatmap, **kwargs)
     ax.invert_yaxis()
 
-    # Set the ticks.
     ax.set_xticks(list(ticks.keys()))
     ax.set_xticklabels(list(ticks.values()))
     ax.set_yticks(np.arange(7))
-    ax.set_yticklabels(DAYS)
+    ax.set_yticklabels(days)
 
-    # Set the current image and axes in the pyplot API.
     plt.sca(ax)
     plt.sci(mesh)
 
     return ax
 
-
-def date_heatmap_demo():
-    '''An example for `date_heatmap`.
-
-    Most of the sizes here are chosen arbitrarily to look nice with 1yr of
-    data. You may need to fiddle with the numbers to look right on other data.
-    '''
-    # Get some data, a series of values with datetime index.
-    data = np.random.randint(5, size=1369)
-    data = pd.Series(data)
-    data.index = pd.DatetimeIndex(start='2015-01-01', end='2018-09-30', freq='1D')
-
-    # Create the figure. For the aspect ratio, one year is 7 days by 53 weeks.
-    # We widen it further to account for the tick labels and color bar.
-    figsize = plt.figaspect(7 / 56)
-    fig = plt.figure(figsize=figsize)
-
-    # Plot the heatmap with a color bar.
-    ax = date_heatmap(data, edgecolor='black')
-    plt.colorbar(ticks=range(5), pad=0.02)
-
-    # Use a discrete color map with 5 colors (the data ranges from 0 to 4).
-    # Extending the color limits by 0.5 aligns the ticks in the color bar.
-    cmap = mpl.cm.get_cmap('Blues', 5)
-    plt.set_cmap(cmap)
-    plt.clim(-0.5, 4.5)
-
-    # Force the cells to be square. If this is set, the size of the color bar
-    # may look weird compared to the size of the heatmap. That can be corrected
-    # by the aspect ratio of the figure or scale of the color bar.
-    ax.set_aspect('equal')
-
-    # Save to a file. For embedding in a LaTeX doc, consider the PGF backend.
-    # http://sbillaudelle.de/2015/02/23/seamlessly-embedding-matplotlib-output-into-latex.html
-    fig.savefig('heatmap.pdf', bbox_inches='tight')
-
-    # The firgure must be explicitly closed if it was not shown.
-    plt.show(fig)
-
 def gym_heatmap():
-    # Get some data, a series of values with datetime index.
     dates = pd.date_range(start='2015-01-01', end='2018-12-31')
     data = pd.read_csv('~/Desktop/Gym-calandar-heat-map/all_data.csv')
-    #data["Date_Time"] = data["Date"].map(str) + " " + data["Time_Numerical"].map(str)
     data = dict(zip(data['Date'], data['Time_Numerical']))
     print(data)
     data = pd.Series(data)
     data.index = pd.DatetimeIndex(data.index)
-    #data = pd.to_datetime(data)
-    #data = data.set_index(data)
     data = data.reindex(dates, fill_value=-100)
-    #print(data.head(5))
-    #data = pd.Series(data["Date_Time"])
 
-    #data.index = pd.DatetimeIndex(data, start='2015-01-01', end='2018-09-30')
-
-    # Create the figure. For the aspect ratio, one year is 7 days by 53 weeks.
-    # We widen it further to account for the tick labels and color bar.
     figsize = plt.figaspect(7 / 56*3)
     fig = plt.figure(figsize=figsize)
 
-    # Plot the heatmap with a color bar.
-    ax = date_heatmap(data, edgecolor='black')
+    ax = calendar_heatmap(data, edgecolor='black')
     plt.colorbar(ticks=range(100), pad=0.02)
 
-    # Use a discrete color map with 5 colors (the data ranges from 0 to 4).
-    # Extending the color limits by 0.5 aligns the ticks in the color bar.
     cmap = mpl.cm.get_cmap('cividis', 100)
     plt.set_cmap(cmap)
-    plt.clim(0.30, 0.95)
+    plt.clim(0.1, .99)
 
-    # Force the cells to be square. If this is set, the size of the color bar
-    # may look weird compared to the size of the heatmap. That can be corrected
-    # by the aspect ratio of the figure or scale of the color bar.
-    #ax.set_aspect('equal')
-
-    # Save to a file. For embedding in a LaTeX doc, consider the PGF backend.
-    # http://sbillaudelle.de/2015/02/23/seamlessly-embedding-matplotlib-output-into-latex.html
-    fig.savefig('heatmap.pdf', bbox_inches='tight')
-
-    # The firgure must be explicitly closed if it was not shown.
+    fig.savefig('heatmap.png', bbox_inches='tight')
     plt.show(fig)
 
 
 if __name__ == "__main__":
 
-    #date_heatmap_demo()
     gym_heatmap()
